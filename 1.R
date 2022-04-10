1a6e9b261b0c9a2c946f29a593e8888d83122919
adifachanda <79835729+adifachanda@users.noreply.github.com>
2022-04-10 15:20:18 +0700

    Update 1.R

--- 1.R
+++ 1.R
@@ -1,28 +1,30 @@
 #Nomor 1a
-> dgeom(3, .20)
+x = 3
+prob = .2
+dgeom(x, prob)
 
 #Nomor 1b
-> mean(rgeom(n = 10000, .20) == 3)
+mean(rgeom(n = 10000, prob) == x)
 
 #Nomor 1d
-> library(dplyr)
-> library(ggplot2)
-> data.frame(x = 0:10, prob = dgeom(x = 0:10, .20)) %>%
-+     mutate(Failures = ifelse(x == 3, 3, "other")) %>%
-+     ggplot(aes(x = factor(x), y = prob, fill = Failures)) +
-+     geom_col() +
-+     geom_text(
-+         aes(label = round(prob,2), y = prob + 0.01),
-+         position = position_dodge(0.9),
-+         size = 3,
-+         vjust = 0
-+     ) +
-+     labs(title = "Probability of X = 3 Failures Prior to First Success",
-+          subtitle = "Geometric(.2)",
-+          x = "Failures prior to first success (x)",
-+          y = "Probability") 
+library(dplyr)
+library(ggplot2)
+data.frame(x = 0:10, prob = dgeom(x = 0:10, prob)) %>%
+     mutate(Failures = ifelse(x == 3, 3, "other")) %>%
+     ggplot(aes(x = factor(x), y = prob, fill = Failures)) +
+     geom_col() +
+     geom_text(
+         aes(label = round(prob,2), y = prob + 0.01),
+         position = position_dodge(0.9),
+         size = 3,
+         vjust = 0
+     ) +
+     labs(title = "Probability of X = 3 Failures Prior to First Success",
+          subtitle = "Geometric(.2)",
+          x = "Failures prior to first success (x)",
+          y = "Probability") 
 
 #Nomor 1e
-> mean(rgeom(n = 10000, .20)) + 1
-> var(rgeom(n = 100000, .20))
+mean(rgeom(n = 10000, prob)) + 1
+var(rgeom(n = 100000, prob))
 
