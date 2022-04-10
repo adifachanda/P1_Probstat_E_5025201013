da861bfc26d981147be55b99e9e394f70985834b
adifachanda <79835729+adifachanda@users.noreply.github.com>
2022-04-10 15:42:32 +0700

    Create 5.R

---
+++ 5.R
@@ -0,0 +1,17 @@
+x = 3
+
+#Nomor 5a
+dexp(x, rate=1,log=FALSE)
+
+#Nomor 5b
+hist(rexp(10))
+hist(rexp(100))
+hist(rexp(1000))
+hist(rexp(10000))
+
+#Nomor 5c
+n = 100
+mean = 1 / x
+mean
+var = ((mean) * (1 / sqrt(n))) * ((mean) * (1 / sqrt(n)))
+var
