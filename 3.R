22997953ec7b60e6a6570327867167b5bfdb8825
adifachanda <79835729+adifachanda@users.noreply.github.com>
2022-04-10 19:16:52 +0700

    Update 3.R

--- 3.R
+++ 3.R
@@ -5,8 +5,23 @@ x = 6
 dpois(x, lambda)
 
 #Nomor 3b
-n = 365
-hist(rpois(n, lambda), main = "Histogram Poisson")
+library(dplyr)
+library(ggplot2)
+set.seed(2)
+
+babies <- data.frame('data' = rpois(365, 4.5))
+
+babies %>% ggplot() +
+  geom_histogram(aes(x = data,
+                     y = stat(count / sum(count)),
+                     fill = data == 6),
+                 binwidth = 1,
+                 color = 'black',) +
+  scale_x_continuous(breaks = 0:10) + 
+  labs(x = 'Number of babies born per period',
+       y = 'Proportion',
+       title = '365 simulated births in a hospital with Pois(lambda = 4.5)') +
+  theme_bw()
 
 #Nomor 3d
 mean = lambda
